4c83a28ee5f49fdd21f690f8cfa69ad6b2cecbef
Benjamin Söllner <benjamin.soellner@gmail.com>
2016-01-10 18:26:08 +0100

    re-structured everything

--- github_commit_to_pages.bat
+++
@@ -1,3 +0,0 @@
-rem See https://help.github.com/articles/creating-project-pages-manually/
-git checkout --orphan gh-pages
-git commit
--- jekyll_serve.bat
+++ jekyll_serve.bat
@@ -1,4 +1,4 @@
 rem Execute from Ruby command prompt, open shell like this
 rem C:\Windows\System32\cmd.exe /E:ON /K "C:\Unmanaged Programs\Ruby22\bin\setrbvars.bat"
 cd %~dp0
-jekyll serve --destination blog --source blog_src
+jekyll serve --destination .\_site --source .
